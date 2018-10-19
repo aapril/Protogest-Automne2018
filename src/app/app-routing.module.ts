cadd268c5a2869381f6500882a1fcdeabf58d277
PhilN <philippe.d.b.ngo@gmail.com>
2018-10-19 16:15:11 -0400

    Added base routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventsComponent } from './events/events/events.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './welcome/login/login.component';
+import { ResetPasswordComponent } from './welcome/reset-password/reset-password.component'; 
+ 
+
+const routes = [
+  { path: '/events', component: EventsComponent },
+  { path: '/signup', component: SignupComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/reset', component: ResetPasswordComponent }
+]
 
 @NgModule({
   imports: [
