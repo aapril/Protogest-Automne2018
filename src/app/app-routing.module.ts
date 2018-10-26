baebcc78f7df2a1c93c94342b9669f7db3a93acd
PhilN <philippe.d.b.ngo@gmail.com>
2018-10-26 14:31:14 -0400

    Added a route for the calendar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { EventsComponent } from './events/events/events.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './welcome/login/login.component';
 import { ResetPasswordComponent } from './welcome/reset-password/reset-password.component'; 
+import { CalendarComponent } from './calendar/calendar.component';
  
 
 const routes: Routes = [
   { path: 'events', component: EventsComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'calendar', component: CalendarComponent },
   { path: 'reset', component: ResetPasswordComponent },
   { path: '**', redirectTo: 'events', pathMatch: 'full' }
 ]
