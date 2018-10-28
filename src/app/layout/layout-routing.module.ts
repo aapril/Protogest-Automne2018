0a4a514c14c97ba4fb0bbc552b29a0896bc4cab6
Codio Carl-Henri (Nter) <carlhenri.codio@tech-nter.com>
2018-10-28 14:49:30 -0400

    Change name

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'forms', loadChildren: './form/form.module#FormModule' },
             { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
+            { path: 'grid', loadChildren: './maps/grid.module#GridModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
