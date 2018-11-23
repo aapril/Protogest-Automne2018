af940356aa27e6f609750af734d059a222ce2db3
PhilN <philippe.d.b.ngo@gmail.com>
2018-11-23 10:07:23 -0500

    Fixed date formating in the edit event view. Fixed requests to the API in relation to the event component

--- src/app/layout/events/event.component.ts
+++ src/app/layout/events/event.component.ts
@@ -12,11 +12,7 @@ import { FormGroup, FormControl } from '@angular/forms'
   animations: [routerTransition()]
 })
 export class EventComponent implements OnInit { 
-  eventFormGroup = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-    eventDate: new FormControl(''),
-  })
+  eventFormGroup: FormGroup 
   searchFormGroup = new FormGroup({
     searchValue: new FormControl('')
   })
@@ -26,7 +22,13 @@ export class EventComponent implements OnInit {
   event: Event;
   filterType;
   editView: boolean = false;
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService) {
+    this.eventFormGroup = new FormGroup({
+      name: new FormControl(''),
+      description: new FormControl(''),
+      eventDate: new FormControl('')
+    })
+  }
 
   ngOnInit() {
     this.getEvents();
@@ -35,109 +37,7 @@ export class EventComponent implements OnInit {
   }
 
   getEvents(): void {
-    //this.eventService.getUserEvents().subscribe(events => this.events = events)
-    this.events = [
-      {
-        id: 0,
-        name: "Event1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 1,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 1,
-        name: "Event2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 2,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 2,
-        name: "Event3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 2,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 3,
-        name: "Event4",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 2
-      },
-      {
-        id: 4,
-        name: "Event5",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 3
-      },
-      {
-        id: 5,
-        name: "Event6",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 6,
-        name: "Event7",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 7,
-        name: "Event8",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 8,
-        name: "Event9",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 1
-      },
-      {
-        id: 9,
-        name: "Event10",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        eventDate: new Date("2018-11-15T17:13:44.855Z"),
-        eventGroupId: 1,
-        taskId: 3,
-        authorId: 1,
-        eventStateId: 1
-      }
-    ]
+    this.eventService.getUserEvents().subscribe(events => this.events = events)
   }
 
   filterEvents(filter: string){
@@ -167,20 +67,6 @@ export class EventComponent implements OnInit {
     this.eventService.getEvents().subscribe(event => this.events = event)
   }
 
-  updateEvent() {
-    this.event.name = this.eventFormGroup.value.name;
-    this.event.description = this.eventFormGroup.value.description;
-    this.event.eventDate = new Date(this.eventFormGroup.value.eventDate)
-    this.editView = false;
-    // if (this.event) {
-    //   this.eventService.updateEvent(this.event)
-    //     .subscribe(event => {
-    //       //logic to do
-    //     });
-    //   this.event = undefined;
-    // }
-  }
-
   createEvent(event: Event) {
     if (event){
       this.eventService.createEvent(event)
@@ -194,6 +80,11 @@ export class EventComponent implements OnInit {
 
   editEvent(event: Event){
     this.event = event;
+    this.eventFormGroup.patchValue({
+      name: this.event.name,
+      description: this.event.description,
+      eventDate: this.event.eventDate
+   });
     this.editView = true;
   }
   
@@ -206,10 +97,28 @@ export class EventComponent implements OnInit {
     event.eventStateId = 3;
     // this.events = this.eventFilterPipe.transform(this.events, this.currentFilter);
     //this.events = this.events.filter(e => e !== event);
-    //this.eventService.deleteEvent(event.id).subscribe();
+    this.eventService.deleteEvent(event.id).subscribe();
   }
 
   searchEvent() {
     this.search = this.searchFormGroup.value.searchValue;
+  }
+
+  updateEvent() {
+    this.event.name = this.eventFormGroup.value.name;
+    this.event.description = this.eventFormGroup.value.description;
+    if(this.eventFormGroup.value.eventDate.year != null && this.eventFormGroup.value.eventDate.month != null  && this.eventFormGroup.value.eventDate.day != null)
+      this.event.eventDate = new Date(this.eventFormGroup.value.eventDate.year, this.eventFormGroup.value.eventDate.month - 1, this.eventFormGroup.value.eventDate.day)
+    else
+      this.event.eventDate = this.eventFormGroup.value.eventDate
+    console.log(this.event.eventDate)
+    console.log(this.event)
+    if (this.event) {
+      this.eventService.updateEvent(this.event)
+        .subscribe(event => {
+          this.event = event
+          this.editView = false
+        });
+    }
   }
 }
