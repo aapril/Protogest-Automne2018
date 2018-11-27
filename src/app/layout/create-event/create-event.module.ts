906d028f3c616dcf2bea3aea20e30e5bfdfbd67a
Albert Dekermenjian <albert.dekermenjian@outlook.com>
2018-11-26 22:17:16 -0500

    added the datepicker

--- src/app/layout/create-event/create-event.module.ts
+++ src/app/layout/create-event/create-event.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CreateEventRootingModule } from './create-event-rooting.module';
 import { CreateEventComponent } from './create-event.component';
 import { PageHeaderModule } from './../../shared';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-    imports: [CommonModule, CreateEventRootingModule, PageHeaderModule],
+    imports: [CommonModule, CreateEventRootingModule, PageHeaderModule, NgbModule],
   declarations: [CreateEventComponent]
 })
 export class CreateEventModule { }
