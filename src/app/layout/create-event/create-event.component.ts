024ba5163632fd06ed8b9c169295fdeb9640bafa
Albert Dekermenjian <albert.dekermenjian@outlook.com>
2018-12-08 13:23:34 -0500

    date-time logic is correctly done

--- src/app/layout/create-event/create-event.component.ts
+++ src/app/layout/create-event/create-event.component.ts
@@ -23,9 +23,18 @@ export class CreateEventComponent implements OnInit {
         name: new FormControl('', Validators.required),
         date: new FormControl('', Validators.required),
         description: new FormControl('', Validators.required),
+        hours: new FormControl('', Validators.required),
+        minutes: new FormControl('', Validators.required),
+        seconds: new FormControl('', Validators.required)
         });
 
     errorMessage: boolean;
+
+    eventDateTime: string;
+    eventYear: string;
+    eventMonth: string;
+    eventDay: string;
+
 
   constructor(private eventService: EventService, private router: Router) { }
 
@@ -53,10 +62,19 @@ export class CreateEventComponent implements OnInit {
       if (!this.createEventForm.get('name').invalid && !this.createEventForm.get('date').invalid
       && !this.createEventForm.get('description').invalid) {
 
+          console.log(this.createEventForm.get('date').value);
+
+        this.eventDateTime = this.createEventForm.get('date').value.year + '-' + this.createEventForm.get('date').value.month + '-' +
+            this.createEventForm.get('date').value.day + 'T' + this.createEventForm.get('hours').value + ':' +
+            this.createEventForm.get('minutes').value + ':' + this.createEventForm.get('seconds').value + '.855Z';
+
+
+        console.log(this.eventDateTime);
+
           this.event = {
               name: this.createEventForm.get('name').value,
               description: this.createEventForm.get('description').value,
-              eventDate: '2018-11-11T18:18:48.855Z',
+              eventDate: this.eventDateTime,
               eventGroupId: 4,
               memberId: 25
           };
