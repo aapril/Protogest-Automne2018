291bc638120e517410a004309bfb9348cbab64e1
PhilN <philippe.d.b.ngo@gmail.com>
2018-12-09 18:59:50 -0500

    Removed duplicate imports. Added scheduler with configs with mock data

--- src/app/layout/calendar/calendar.component.ts
+++ src/app/layout/calendar/calendar.component.ts
@@ -12,19 +12,56 @@ import { routerTransition } from '../../router.animations';
 export class CalendarComponent implements OnInit, AfterViewInit {
   @ViewChild("calendar") calendar : DayPilotCalendarComponent;
 
-  events: any[];
-
   calendarConfig = {
-    startDate: DayPilot.Date.today(),
-    viewType: "Week"
+    locale: "en-ca",
+    timeHeaders: [
+      {
+        "groupBy": "Day"
+      },
+      {
+        "groupBy": "Hour"
+      }
+    ],
+    scale: "Hour",
+    days: DayPilot.Date.today().daysInMonth(),
+    startDate: DayPilot.Date.today().firstDayOfMonth(),
+    businessWeekends: true,
+    eventHeight: 30,
+    timeRangeSelectedHandling: "Disabled",
+    eventMoveHandling: "Disabled",
+    eventResizeHandling: "Disabled",
+    eventDeleteHandling: "Disabled",
+    eventClickHandling: "Select",
+    onEventEdited: function (args) {
+      this.message("Event selected: " + args.e.text());
+    },
+    eventHoverHandling: "Bubble",
+    bubble: new DayPilot.Bubble({
+      onLoad: function(args) {
+        // if event object doesn't specify "bubbleHtml" property 
+        // this onLoad handler will be called to provide the bubble HTML
+        args.html = "Event details";
+      }
+    }),
+    resources: [
+      {
+        "name": "Event",
+        "id": "R1"
+      }
+    ]
   };
+
+  events: any[] = [
+    { id: 1, start: "2018-12-09", end: "2018-12-12", resource: "R1", text: "Event 1" }
+  ]
+
 
   constructor(private ds: CalendarService) {  }
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.ds.getEvents(this.calendar.control.visibleStart(), this.calendar.control.visibleEnd()).subscribe(result => this.events = result);
+    //this.ds.getEvents(this.calendar.control.visibleStart(), this.calendar.control.visibleEnd()).subscribe(result => this.events = result);
   }
 
 }
