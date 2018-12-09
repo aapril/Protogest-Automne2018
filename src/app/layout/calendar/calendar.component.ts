043dd93bc21c517f2f18ec961aaa161ab59473d5
Codio Carl-Henri (Nter) <carlhenri.codio@tech-nter.com>
2018-12-09 17:37:20 -0500

    update code component calendar

--- src/app/layout/calendar/calendar.component.ts
+++ src/app/layout/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, AfterViewInit} from "@angular/core";
+import {Component, ViewChild, OnInit, AfterViewInit} from '@angular/core';
 import {DayPilot, DayPilotCalendarComponent, DayPilotNavigatorComponent} from "daypilot-pro-angular";
 import {CalendarService} from "./calendar.service";
 import { routerTransition } from '../../router.animations';
@@ -9,63 +9,22 @@ import { routerTransition } from '../../router.animations';
   styleUrls: ['./calendar.component.scss'],
   animations: [routerTransition()]
 })
-export class CalendarComponent implements AfterViewInit {
+export class CalendarComponent implements OnInit, AfterViewInit {
+  @ViewChild("calendar") calendar : DayPilotCalendarComponent;
 
-  @ViewChild("navigator") navigator: DayPilotNavigatorComponent;
-  @ViewChild("calendar") calendar: DayPilotCalendarComponent;
+  events: any[];
 
-  expanded: boolean = true;
-
-  navigatorConfig: any = {
-    showMonths: 3,
-    skipMonths: 3,
-    selectMode: "week",
-    cellWidth: 30,
-    cellHeight: 30,
-    dayHeaderHeight: 30,
-    titleHeight: 30
+  calendarConfig = {
+    startDate: DayPilot.Date.today(),
+    viewType: "Week"
   };
 
-  events: any[] = [];
+  constructor(private ds: CalendarService) {  }
 
-  config: any = {
-    startDate: "2018-07-01",
-    viewType: "Week",
-    heightSpec: "Parent100Pct",
-    cellHeight: 30,
-    headerHeight: 30,
-    hourWidth: 60,
-    onTimeRangeSelected: function (args) {
-      DayPilot.Modal.prompt("Create a new event:", "Event 1").then(function(modal) {
-        var dp = args.control;
-        dp.clearSelection();
-        if (!modal.result) { return; }
-        dp.events.add(new DayPilot.Event({
-          start: args.start,
-          end: args.end,
-          id: DayPilot.guid(),
-          text: modal.result
-        }));
-      });
-    }
-  };
-
-  constructor(private ds: CalendarService) {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    this.ds.getEvents(this.calendar.control.visibleStart(), this.calendar.control.visibleEnd()).subscribe(result => this.events = result);
   }
-
-  viewChange(): void {
-
-    var from = this.calendar.control.visibleStart();
-    var to = this.calendar.control.visibleEnd();
-
-    this.ds.getEvents(from, to).subscribe(result => {
-      this.events = result;
-    });
-  }
-
-  ngOnInit() {}
 
 }
