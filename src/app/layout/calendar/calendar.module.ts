bb145432e9d1d1601b85d9c165ead9fe1da1abb1
PhilN <philippe.d.b.ngo@gmail.com>
2018-12-09 18:59:50 -0500

    Removed duplicate imports. Added scheduler with configs with mock data

--- src/app/layout/calendar/calendar.module.ts
+++ src/app/layout/calendar/calendar.module.ts
@@ -3,25 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar.component';
-import { PageHeaderModule } from './../../shared';
 
 import {CalendarService} from "./calendar.service";
 import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
 import {DayPilotModule} from "daypilot-pro-angular";
 import {HttpClientModule} from "@angular/common/http";
-//import {SidebarModule} from "../sidebar/sidebar.module";
 
 @NgModule({
     imports: [
       DayPilotModule, 
-      BrowserModule,
       FormsModule,
       HttpClientModule, 
       CommonModule, 
-      CalendarRoutingModule, 
-      PageHeaderModule
-      //SidebarModule
+      CalendarRoutingModule 
     ],
     declarations: [CalendarComponent],
     
