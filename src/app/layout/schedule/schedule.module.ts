10eb454504c5394ac4ec494207336369a555305b
Julien Métivier <julien.metivier@bonlook.com>
2019-02-07 15:26:20 -0500

    Calendar works

--- src/app/layout/schedule/schedule.module.ts
+++ src/app/layout/schedule/schedule.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { FullCalendarModule } from 'ng-fullcalendar';
 import { CommonModule } from '@angular/common';
 
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { ScheduleComponent } from './schedule.component';
+import { EventService } from './event.service';
 
 @NgModule({
   imports: [
     CommonModule,
-    ScheduleRoutingModule
+    ScheduleRoutingModule,
+    FormsModule,
+    FullCalendarModule
   ],
-  declarations: [ScheduleComponent]
+  declarations: [ScheduleComponent],
+  providers: [ EventService ]
 })
 export class ScheduleModule { }
